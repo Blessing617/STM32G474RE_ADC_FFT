22307b19f662c3207d1bb79eaa6549bd4c19bfda
Blessing <535104678@qq.com>
2025-03-11 23:49:42 +0800

    test

---
+++ MDK-ARM/G474_temp/windowfunction.d
@@ -0,0 +1,5 @@
+g474_temp\windowfunction.o: ..\HARDWARE\Window\WindowFunction.c
+g474_temp\windowfunction.o: ..\HARDWARE\Window\WindowFunction.h
+g474_temp\windowfunction.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+g474_temp\windowfunction.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+g474_temp\windowfunction.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
